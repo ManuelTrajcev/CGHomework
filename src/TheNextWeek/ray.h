45faac5cc689286b869e5d35e4edaf1671ceb353
ManuelTrajcev <manueltrajcev7@gmail.com>
2024-08-24 13:37:07 +0200

    Time in ray and camera

--- src/TheNextWeek/ray.h
+++ src/TheNextWeek/ray.h
@@ -7,8 +7,13 @@ class ray {
 public:
     ray() {}
 
-    ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+    ray(const point3& origin, const vec3& direction, double time)
+        : orig(origin), dir(direction), tm(time) {}
 
+    ray(const point3& origin, const vec3& direction)
+        : ray(origin, direction, 0) {}
+
+    double time() const { return tm; }
     const point3& origin() const { return orig; }
     const vec3& direction() const { return dir; }
 
@@ -19,6 +24,7 @@ public:
 private:
     point3 orig;
     vec3 dir;
+    double tm;  //[0,1] for 1 frame
 };
 
 #endif
