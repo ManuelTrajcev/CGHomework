1e4579b5f6deee59a6db9891b54286861876b3b2
ManuelTrajcev <manueltrajcev7@gmail.com>
2024-08-27 15:46:56 +0200

    ray-plane intersection

--- src/InOneWeekend/quad.h
+++ src/InOneWeekend/quad.h
@@ -9,6 +9,10 @@ class quad : public hittable {
 public:
     quad(const point3& Q, const vec3& u, const vec3& v, shared_ptr<material> mat) : Q(Q), u(u), v(v), mat(mat)
     {
+        auto n = cross(u, v);
+        normal = unit_vector(n);
+        D = dot(normal, Q);
+        w = n / dot(n, n);
         set_bounding_box();
     }
 
@@ -21,7 +25,22 @@ public:
     aabb bounding_box() const override { return bbox; }
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-        return false; // TODO
+        auto denom = dot(normal, r.direction());
+        if (std::fabs(denom) < 1e-8)    // ray - parallel to the plane
+            return false;
+
+        auto t = (D - dot(normal, r.origin())) / denom;     // t is outside the ray interval
+        if (!ray_t.contains(t))
+            return false;
+
+        auto intersection = r.at(t);
+
+        rec.t = t;
+        rec.p = intersection;
+        rec.mat = mat;
+        rec.set_face_normal(r, normal);
+
+        return true;
     }
 
 private:
@@ -29,6 +48,9 @@ private:
     vec3 u, v;
     shared_ptr<material> mat;
     aabb bbox;
+    vec3 normal;
+    double D;
+    vec3 w;
 };
 
 #endif
