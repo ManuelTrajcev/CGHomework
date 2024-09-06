8d37ee9fa40c8ff43572c3628dec0c1a280a10a7
ManuelTrajcev <manueltrajcev7@gmail.com>
2024-09-06 12:37:09 +0200

    fix for over-brightening

--- src/InOneWeekend/integrate_x_sq.h
+++ src/InOneWeekend/integrate_x_sq.h
@@ -2,12 +2,12 @@
 #include <iostream>
 #include <iomanip>
 
-double icd(double d) {
-    return 2.0 * d;
+double icd(double d) {      //randomly samble values from the distribution
+    return std::sqrt(4.0 * d);
 }
 
 double pdf(double x) {
-    return 0.5;
+    return x / 2.0;
 }
 
 int main() {
@@ -17,8 +17,12 @@ int main() {
     auto sum = 0.0;
 
     for (int i = 0; i < N; i++) {
-        auto x = icd(random_double());
-        sum += x * x / pdf(x);
+        auto z = random_double();
+        if (z == 0.0)  // Ignore zero to avoid NaNs
+            continue;
+
+        auto x = icd(z);
+        sum += x * x / pdf(x);      
     }
 
     std::cout << std::fixed << std::setprecision(12);
